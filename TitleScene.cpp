f5680bf892c73d7d4862d88eac1a6d1756c4be8b
大和 遠藤 <230254@jc-21.jp>
2025-02-10 16:45:39 +0900

    上げる

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,15 +1,7 @@
 #include "TitleScene.h"
-#include "Engine/SceneManager.h"
-#include "DxLib.h"
-
-inline bool operator == (const IPDATA& a, const IPDATA& b) {
-    if (a.d1 == b.d1 && a.d2 == b.d2 && a.d3 == b.d3 && a.d4 == b.d4)return  true; 
-    return false;
-
-};
 
 TitleScene::TitleScene(GameObject* parent)
-    : GameObject(parent, "TitleScene"), NetUDPHandle(-1), isCircleReceived(false) {
+    : GameObject(parent, "TitleScene"){
    
 }
 
